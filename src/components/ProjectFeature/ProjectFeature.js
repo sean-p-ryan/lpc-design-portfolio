f896a036a1a5e40b50c0383f7f4ee945b09771d6
Sean Ryan <seanpryan89@gmail.com>
2020-03-29 12:33:16 -0400

    project feature section pulling in images properly

--- src/components/ProjectFeature/ProjectFeature.js
+++ src/components/ProjectFeature/ProjectFeature.js
@@ -1,14 +1,30 @@
 import React from "react";
+import "./project-feature.css"
 
 class ProjectFeature extends React.Component {
   constructor(props) {
-    super(props);    
+    super(props);
   }
 
   render() {
-      const data = this.props.data[this.props.match.params.id]
-      console.log(data);
-  return <div>{data.title}</div>;
+    const data = this.props.data[this.props.match.params.id];    
+
+    return (
+      <div className="feature-outer-wrapper">
+        <div className="description">
+          <div>{data.title}</div>
+          <div>{data.feature.description}</div>
+        </div>
+        <div className="feature-images">
+            {data.feature.images.map((image, i) => {
+                const background = require(`../../img/${image}`);
+                return (
+                    <div className="feature-image" style={{backgroundImage: `url('${background}')`}}></div>
+                )
+            })}
+        </div>
+        </div>
+    );
   }
 }
 
--- src/components/ProjectPreview/ProjectPreview.js
+++ src/components/ProjectPreview/ProjectPreview.js
@@ -1,15 +1,15 @@
 import React from "react";
 import "./project-preview.css";
-import styled from 'styled-components'
+import styled from "styled-components";
 
 class ProjectPreview extends React.Component {
   constructor(props) {
-    super(props);    
+    super(props);
   }
 
   render() {
     const data = this.props.data;
-    const backgroundImage = require(`../../img/${data.backgroundImage}`);        
+    const backgroundImage = require(`../../img/${data.backgroundImage}`);
 
     let projectPreviewStyle = {
       width: "33.33vw",
@@ -18,8 +18,8 @@ class ProjectPreview extends React.Component {
       backgroundPosition: "center",
       backgroundSize: "cover",
       backgroundRepeat: "no-repeat"
-    };  
-    
+    };
+
     const HoverText = styled.p`
     opacity: 0.0    
     width: 100%;
@@ -30,13 +30,10 @@ class ProjectPreview extends React.Component {
 `
 
     return (
-      <div 
-        style={projectPreviewStyle}
-        className="project-preview-box"
-        >                
-        <div className="overlay"> 
-        <h4 className="project-title">{data.title}</h4>       
-        </div>        
+      <div style={projectPreviewStyle} className="project-preview-box">
+        <div className="overlay">
+          <h4 className="project-title">{data.title}</h4>
+        </div>
       </div>
     );
   }
