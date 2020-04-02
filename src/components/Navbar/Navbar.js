a040adfac5d0a351c30e04bf7d06acfe8f962b37
Sean Ryan <seanpryan89@gmail.com>
2020-04-02 17:47:43 -0400

    Nav works properly at all breakpoints

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,22 +1,83 @@
 import React from "react";
-import { Link } from "react-router-dom";
-import "./navbar.css";
+import { NavLink } from "react-router-dom";
+import "./navbar.scss";
 
 class Navbar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      mobileNavHidden: null
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  componentDidMount() {
+    window.addEventListener("resize", this.setLargeWidthNav.bind(this));
+    window.addEventListener("click", this.setLargeWidthNav.bind(this));
+    this.setInitialState.bind(this)
+  }
+
+  setInitialState() {    
+    if (window.innerWidth <= 640) {
+      this.setState({mobileNavHidden: true})
+    } else if (window.innerWidth > 640) {
+      this.setState({mobileNavHidden: false})
+    }
+  }
+
+  setLargeWidthNav(event) {
+    console.log(event.type)
+    if (window.innerWidth >= 640) {
+      this.setState({ mobileNavHidden: false })
+    } else if (window.innerWidth < 640 && event.type == "resize")  {
+      this.setState({ mobileNavHidden: true })
+    }
+  }
+
+  handleClick() {    
+    const currentState = (this.state.mobileNavHidden)
+    this.setState({ mobileNavHidden: !currentState})  
+  }
+
   render() {
     return (
-      <div className="navbar-container">        
-          <div className="nav-item-container">
-            <Link to="/about">
-              <div className="nav-item">LPC</div>
-            </Link>
-            <Link to="/">
-              <div className="nav-item">COVERS</div>
-            </Link>
-            <Link to="/work">
-              <div className="nav-item">PRINT & IDENTITY</div>
-            </Link>
-          </div>        
+      <div className="navbar-container">
+        <div onClick={this.handleClick} className="mobile-nav-menu">MENU</div>
+        <div         
+          className={
+            (this.state.mobileNavHidden) ?
+              "nav-item-container-hidden" : 
+              "nav-item-container-visible" 
+            }   
+        >
+          <NavLink
+            onClick={this.handleClick}
+            activeClassName="selected"
+            className="nav-item"
+            exact
+            to="/about"
+          >
+            LPC
+          </NavLink>
+          <NavLink
+            onClick={this.handleClick}
+            activeClassName="selected"
+            className="nav-item"
+            exact
+            to="/"
+          >
+            COVERS
+          </NavLink>
+          <NavLink
+            onClick={this.handleClick}
+            activeClassName="selected"
+            className="nav-item"
+            exact
+            to="/work"
+          >
+            PRINT & IDENTITY
+          </NavLink>
+        </div>
       </div>
     );
   }
