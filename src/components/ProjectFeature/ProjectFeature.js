c4f4c1965b0f6b069353bdb8fd730635ee21cba8
Sean Ryan <seanpryan89@gmail.com>
2020-04-04 15:12:29 -0400

    Added navigation arrows

--- src/components/ProjectFeature/ProjectFeature.js
+++ src/components/ProjectFeature/ProjectFeature.js
@@ -63,7 +63,11 @@ class ProjectFeature extends React.Component {
             );
           })}
         </div>
-        <div className="arrows">ARROWS</div>
+        <div className="nav-arrow-container">
+          <div className="last"></div>
+          <div className="home"></div>
+          <div className="next"></div>
+        </div>
       </div>
     );
   }
